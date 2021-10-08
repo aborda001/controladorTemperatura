b420c09ef85c09e4261453a71d7ee1b36fe64a6e
Arnaldo Borda <aborda001@gmail.com>
2021-10-07 21:41:58 -0300

    Last commit xd

--- controladorTemperatura.ino
+++ controladorTemperatura.ino
@@ -3,27 +3,32 @@
 #include "RTClib.h"
 
 RTC_DS1307 rtc;
-Thermistor temp(0);
-double resistor = 10000;
-int sample = 5000;
-int pins[5] = {2,3,4,5,6};
-
+Thermistor temp(0); //begin object Thermistor in pin 0
+double resistor = 10000; //value of resistor in ohms
+int sample = 5000; //number of samples
+int pins[5] = {2, 3, 4, 5, 6}; //pins to use
+int pinsSize = 5; //pins array size
+int tempMax = 25; //max temp to turn on the pins
+int firstCycle[2] = {6,8}; //first cycle to turn on the pins
+int secondCycle[2] = {21,22}; //second cycle to turn on the pins
+long int interval = 3000; //time interval between switching on the pins
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-
-  for (int pin = 0; pin < 5; pin++) {
-    pinMode(pins[pin], OUTPUT);
-    digitalWrite(pins[pin], LOW);
-  }
-    
   Serial.begin(9600);
   rtc.begin();
-
+  
   if (rtc.isrunning()) {
+    //sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   } else {
     Serial.println("RTC is not running");
+  }
+
+  for (int pin = 0; pin < pinsSize; pin++) {
+    //initialize pins to 'LOW' and 'OUTPUT' mode
+    pinMode(pins[pin], OUTPUT);
+    digitalWrite(pins[pin], LOW);
   }
 
   lcd.init();
@@ -34,7 +39,7 @@ void setup() {
   lcd.print("Controlador de");
   lcd.setCursor(0, 1);
   lcd.print("temperatura.");
-  delay(1000);
+  delay(3000);
   lcd.clear();
 }
 
@@ -43,24 +48,37 @@ void loop() {
   String hora = time.timestamp(DateTime::TIMESTAMP_TIME);
   char buf1[] = "mm";
   char buf2[] = "hh";
-  String minutos = time.toString(buf1);
-  String horario = time.toString(buf2);
-  int mm = minutos.toInt();
-  int hh = horario.toInt();
+  String minutes = time.toString(buf1);
+  String hours = time.toString(buf2);
+  int mm = minutes.toInt();
+  int hh = hours.toInt();
 
-  Serial.println("Hora: ");
-  Serial.println(hh);
-  Serial.println("Minutos:  ");
-  Serial.println(mm);
+  double averageTemp = Temperatura();
+  putTime(mm, hh);
 
-  
-  Temperatura();
+  if ((hh >= firstCycle[0]) && (hh <= firstCycle[1])) {
+    if (averageTemp > tempMax) {
+      for (int i = 0; i < pinsSize; i++) {
+        ON(pins[i], interval);
+        OFF(pins[i]);
+        putTime(mm, hh);
+      }
+    }
+  }
+  if ((hh >= secondCycle[0]) && (hh <= secondCycle[1])) {
+    if (averageTemp > tempMax) {
+      for (int i = 0; i < pinsSize; i++) {
+        ON(pins[i], interval);
+        OFF(pins[i]);
+        putTime(mm, hh);
+      }
+    }
+  }
 }
 
 
-void Temperatura() {
+double Temperatura() {
   double average = 0;
-
   //Read the data and plus to variable average
   for (int index = 0; index < sample; index++) {
     double temperature = temp.getTemp(resistor);
@@ -70,21 +88,36 @@ void Temperatura() {
   }
   // get the average
   average = average / sample;
+  //print on LCD the average temperature
   lcd.setCursor(0, 0);
   lcd.print("T:");
   lcd.print(average);
   lcd.print(" C");
   
-  Serial.print("T: ");
-  Serial.print(average);
-  Serial.println(" C");
-  delay(10);
-  //lcd.clear();
+  return average;
 }
 
-void ON(int pin) {
-  digitalWrite(pin,HIGH);
+void ON(int pin, int interval) {
+  //Turn on the pin and print on LCD
+  digitalWrite(pin, HIGH);
+  lcd.setCursor(0, 4);
+  lcd.print("Pin ");
+  lcd.print(pin);
+  lcd.print(" Encendido");
+  delay(interval);
 }
-void OF(int pin) {
-  digitalWrite(pin,LOW);
+
+void OFF(int pin) {
+  //Turn off the pin and clear second row on LCD
+  digitalWrite(pin, LOW);
+  lcd.setCursor(0, 1);
+  lcd.print("                    ");
+}
+
+void putTime(int mm, int hh) {
+  //Put time on LCD 
+  lcd.setCursor(11, 0);
+  lcd.print(hh);
+  lcd.print(":");
+  lcd.print(mm);
 }
